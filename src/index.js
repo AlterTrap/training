4d7c2615c115fab64ad2a0c2b8db08ebadc75d94
KhaiNd3 <kekocoten19@gmail.com>
2021-08-04 16:14:50 +0900

    update lai src login

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express()
 const port = 3000
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.urlencoded({ extended: false }));
