08dde8c22567abe65fc0522e64f04936ddcb748e
KhaiNd3 <kekocoten19@gmail.com>
2021-08-07 00:04:31 +0900

    upload feature signupscreen

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const mongodb = require('mongodb');
+const express = require('express');
 const path = require('path');
 
 const app = express()
 const port = 3000
+const mongoConnect = require('../src/database').mongoConnect;
+const getDb = require('../src/database').getDb;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -13,6 +16,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.render('login');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+
+app.post('/signup', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordCfm = req.body.passwordCfm;
+    const cusAcc = {username: username, password: password}
+    const db = getDb();
+    if (password == passwordCfm) {
+        db.collection('users').insertOne(cusAcc);
+        return res.redirect('/index?username=' + username +'&&password=' + password)
+    }
 });
 
 app.post('/login', function (req, res) {
@@ -29,6 +48,10 @@ app.get('/index', function (req, res) {
     res.render('index', {username: username, password: password});
 });
 
-app.listen(port, () => {
-    console.log(`App is running on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`App is running on port ${port}`)
+// })
+
+mongoConnect(() => {
+    app.listen(port);
+  });
