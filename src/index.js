c7434e19b11c6e674758f79aab03c92f6a29b80d
KhaiNd3 <kekocoten19@gmail.com>
2021-08-18 23:23:59 +0900

    fix bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const app = express()
 const port = 3000
 const mongoConnect = require('../src/database').mongoConnect;
-const getDb = require('../src/database').getDb; // gán tên hàm vào biến thôi
+const getDb = require('../src/database').getDb; // Just attach the function name to the variable 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
+app.get('/login', function (req, res) {
     res.render('login');
 });
 
@@ -28,25 +28,44 @@ app.post('/signup', function (req, res) {
     const password = req.body.password;
     const passwordCfm = req.body.passwordCfm;
     const db = getDb();
-    if(username.length < 6 || password.length < 6){
-        res.render('signup', {msg: 'username or password not enough 6 letters'})
+    
+    if(password.charAt(0) !== password.charAt(0).toUpperCase()){
+        res.render('signup', {msg: 'Password require first letter is uppercase'})
     }
+
+    // Check length in Username, Password and Password Comfirm
+    if (username.length < 6) {
+        res.render('signup', {msg: 'Username not enough 6 letters'})
+    }if(password.length < 6) {
+        res.render('signup', {msg: 'Password not enough 6 letters'})
+    }if(passwordCfm.length < 6){
+        res.render('signup', {msg: 'Password comfirm not enough 6 letters'})
+    }
+
+    // Check password and password comfirm
     if (password == passwordCfm) {
+
         db.collection('users').findOne({username})
         .then(user => {
-            if(!(user)){
-                bcrypt.genSalt(10, function(err, salt) {  
-                    bcrypt.hash(password, salt, function(err, hash) {
+            //check user already in DB or not
+            if (!(user)) {
+                bcrypt.genSalt(10)
+                .then(salt => {
+                    // Hash password
+                    bcrypt.hash(password, salt)
+                    .then(hash => {
                         let cusAcc = {username: username, password: hash}
+                        // Save user info to DB
                         db.collection('users').insertOne(cusAcc);
-                        return res.redirect('/index?username=' + username +'&&password=' + password)
-                    });
-                });
-            }else{
-                res.render('signup',{msg: 'username already exist'});
+                        return res.redirect('/index?username=' + username)
+                    })
+                })
+            } else {
+                res.render('signup',{msg: 'Username already exist'});
             }
         })
-    }else{
+
+    } else {
         res.render('signup',{msg: 'Password and Password Comfirm not match'});
     }
 });
@@ -56,31 +75,37 @@ app.post('/login', function (req, res) {
     const password = req.body.password;
     const db = getDb();
     let user = {};
-    if(username.length < 6 || password.length < 6){
-        res.render('login',{msg: "username or password not enough 6 letters"})
+
+    // Check length in Username and Password
+    if (username.length < 6) {
+        res.render('signup', {msg: 'Username not enough 6 letters'})
+    }if(password.length < 6) {
+        res.render('signup', {msg: 'Password not enough 6 letters'})
     }
+
+    // Find info user in DB
     db.collection('users').findOne({username})
-        .then(user => {
-            if (!user) return {msg: "user not exist"}
-                bcrypt.compare(password, user.password, function(error, result){
-            if(error){
+    .then(user => {
+        if (!user) return {msg: "user not exist"}
+
+        // Compare Password in client with password DB
+        bcrypt.compare(password, user.password, function(error, result){
+            if (error) {
                 throw error;
             }
-            if(result){
-                res.redirect('/index?username=' + username +'&&password=' + password)
-            }else{
-                res.render('login',{msg: 'not correct password'})
+            if (result) {
+                res.redirect('/index?username=' + username)
+            } else {
+                res.render('login',{msg: 'Username or Password not correct'})
             }
         })
-        }
-    )
-
+    })
 });
 
 app.get('/index', function (req, res) {
     const username = req.query.username
     const password = req.query.password
-    res.render('index', {username: username, password: password});
+    res.render('index', {username: username});
 });
 
 mongoConnect(() => {
