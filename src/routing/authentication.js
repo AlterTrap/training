a6839ab82da603e9e12fc8273adbc7f41239dff7
KhaiNd3 <kekocoten19@gmail.com>
2021-09-20 23:57:52 +0900

    fix routing

--- src/routing/authentication.js
+++ src/routing/authentication.js
@@ -3,61 +3,9 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 
-
 const getDb = require('../database').getDb;
-const LocalStrategy = require("passport-local").Strategy;
 const checkLength = require('../validate').checkLength;
 const oneUpscalePass = require('../validate').oneUpscalePass;
-
-passport.use(
-    new LocalStrategy({ usernameField: "username" }, function (
-        username,
-        password,
-        done
-    ) {
-        const db = getDb();
-        //match user
-        db.collection("users")
-            .findOne({ username: username })
-            .then((user) => {
-                if (!user) {
-                    return done(null, false, { message: "User not exist" });
-                }
-                //match pass
-                bcrypt.compare(password, user.password, (err, isMatch) => {
-                    if (err) return done(null, false, { message: err });
-
-                    if (isMatch) {
-                        return done(null, user);
-                    } else {
-                        return done(null, false, {
-                            message: "Password incorrect",
-                        });
-                    }
-                });
-            })
-            .catch((err) => {
-                return res.render("login", { msg: err });
-            });
-    })
-);
-
-passport.serializeUser((user, done) => {
-    console.log(
-        "Inside serializeUser callback. User id is save to the session file store here"
-    );
-    return done(null, user.username);
-});
-
-passport.deserializeUser(function (username, done) {
-    const db = getDb();
-    db.collection("users").findOne(
-        { username: username },
-        function (err, user) {
-            return done(err, user);
-        }
-    );
-});
 
 router.get("/login", function (req, res) {
     // If user is already logged in, then redirect
